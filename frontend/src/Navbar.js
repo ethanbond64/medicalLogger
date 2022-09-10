664ee9d1dfbb622de49559712d928b8743491d90
ethanbond64 <ethanbond39@gmail.com>
2022-09-10 13:13:41 -0600

    add router

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+
+    return (
+        <div className={`relative w-full h-36 mb-10`}>
+            <Link to="/">
+                <img src="/images/MainTextLogo.png" className={`h-28 pt-3`} alt="Checkout App Logo" />
+            </Link>
+            <div className={`mt-8 absolute inset-y-0 right-0`}>
+                <a className={`inline-block border rounded py-1 px-3 bg-indigo-900 text-white mr-4`}>
+                    <Link to="/create/video">Upload Files</Link>
+                </a>
+                <div className={`inline-block border rounded py-1 px-3 bg-indigo-900 text-white mr-4`}>
+                    Share
+                </div>
+                <div className={`inline-block border rounded py-1 px-3 bg-indigo-900 text-white mr-4`} >
+                    Help
+                </div>
+                <div className={`inline-block rounded px-3 pt-1 text-white align-bottom`} >
+                    <img src='/images/userIcon.png' className={`max-h-10`}></img>
+                </div>
+            </div>
+            {/* <Link to="/" ><img src={logo} alt="Chia No Code Logo" /></Link> */}
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ frontend/src/ShareForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+function ShareForm() {
+    return (
+        <div></div>
+    );
+}
+
+export default ShareForm;
